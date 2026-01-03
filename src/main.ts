f30860e50d0b7a822cf840d890b9e3c92f8ca7b1
KhanAmaan55 <amaanjk.a000@gmail.com>
2026-01-03 16:47:24 +0530

    feat: add environment configuration and logging setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { isDev } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: isDev
+      ? ['log', 'warn', 'error', 'debug', 'verbose']
+      : ['log', 'warn', 'error'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application running on: ${await app.getUrl()}`);
 }
 bootstrap();
