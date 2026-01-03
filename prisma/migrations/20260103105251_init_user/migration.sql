343ffd345df61f4cb2ae4bb8da5653c5f47d5b51
KhanAmaan55 <amaanjk.a000@gmail.com>
2026-01-03 16:26:35 +0530

    feat: add User model with migration and schema definition

--- prisma/migrations/20260103105251_init_user/migration.sql
+++ prisma/migrations/20260103105251_init_user/migration.sql
@@ -2,9 +2,16 @@
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NULL,
     `name` VARCHAR(191) NULL,
+    `passwordHash` VARCHAR(191) NOT NULL,
+    `refreshTokenHash` VARCHAR(191) NULL,
+    `resetPasswordTokenHash` VARCHAR(191) NULL,
+    `resetPasswordExpiresAt` DATETIME(3) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_username_key`(`username`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
